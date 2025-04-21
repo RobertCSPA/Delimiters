fa1d5558e24b80e89cc57809667ad7a638914d09
RobertCSPA <robertf127@nycstudents.net>
2025-04-21 16:33:46 +0000

    almost

--- Main.java
+++ Main.java
@@ -1,1 +1,8 @@
-
+import java.util.ArrayList;
+public class Main{
+    public static void main(String[] args) {
+        String[] tokens = {"(", "x+y", ")", "*5"};
+        Delimiters d = new Delimiters("(", ")");
+        System.out.println(d.getDelimitersList(tokens));
+    }
+}
